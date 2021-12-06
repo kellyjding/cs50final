f2590fd0100afeac4ee1716b8fedd4ff6e03e420
kellyjding <kellyding@college.harvard.edu>
2021-12-05 20:00:26 -0500

    uh

--- app.py
+++ app.py
@@ -75,7 +75,6 @@ def result():
 
     movielist_dir = cur1.execute("SELECT title FROM movies WHERE (crew LIKE '%quentin tarantino%') OR (crew LIKE '%christopher nolan%') OR (crew LIKE '%wes anderson%')")
     movielist_dir = cur1.fetchall()
-    print(movielist_dir)
     # movielist_user = cur.execute("SELECT name FROM letterboxd WHERE name IN (?)", movielist_dir)
     # movielist_user = cur.fetchall()
     # print(movielist_user)
