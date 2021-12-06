95eda09722e59c468499eb5e53120ccbcd50f704
ravestarr <90078846+ravestarr@users.noreply.github.com>
2021-12-05 20:11:47 -0500

    executemany

--- app.py
+++ app.py
@@ -73,9 +73,9 @@ def result():
     messages = ["" for a in range(INSULTNUM)]
     msgcount = 0
 
-    movielist_dir = cur1.execute("SELECT title FROM movies WHERE (crew LIKE '%quentin tarantino%') OR (crew LIKE '%christopher nolan%') OR (crew LIKE '%wes anderson%')")
+    movielist_dir = cur1.executemany("SELECT title FROM movies WHERE (crew LIKE '%quentin tarantino%') OR (crew LIKE '%christopher nolan%') OR (crew LIKE '%wes anderson%')")
     movielist_dir = cur1.fetchall()
-    movielist_user = cur.execute("SELECT name FROM letterboxd WHERE name IN (?)", movielist_dir)
+    movielist_user = cur.executemany("SELECT name FROM letterboxd WHERE name IN (?)", movielist_dir)
     movielist_user = cur.fetchall()
     print(movielist_user)
     length = len(movielist_user)
